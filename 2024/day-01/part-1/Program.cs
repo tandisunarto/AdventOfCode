6210631e7d116faf01cdc2fb7f911fa87fead0ab
Tandi Sunarto <tandi.sunarto@vaeit.com>
2024-12-08 00:51:19 -0500

    Finished Day One

---
+++ 2024/day-01/part-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Math;
+
+using StreamReader sr = new StreamReader("data.txt");
+
+List<long> listA = new();
+List<long> listB = new();
+
+var lines = sr.ReadToEnd().Split("\n");
+foreach(var line in lines)
+{
+  var data = line.Split("|");
+  listA.Add(Int64.Parse(data[0]));
+  listB.Add(Int64.Parse(data[1]));
+}
+
+listA.Sort();
+listB.Sort();
+
+long totalDistance = 0;
+for(int i=0; i<listA.Count; i++)
+{
+  totalDistance += Abs(listA[i] - listB[i]);
+}
+Console.WriteLine("Total Distance: {0}", totalDistance.ToString());
---
+++ 2024/day-01/part-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using static System.Math;
+
+using StreamReader sr = new StreamReader("data.txt");
+
+List<string> listA = new();
+List<string> listB = new();
+
+var lines = sr.ReadToEnd().Split("\r\n");
+foreach(var line in lines)
+{
+  var data = line.Split("|");
+  listA.Add(data[0].ToString());
+  listB.Add(data[1].ToString());
+}
+
+long similarityScore = 0;
+var listBGroupBy = listB.GroupBy(v => v).ToList();
+
+foreach(var val in listA)
+{
+  var result = listBGroupBy.Find(v => v.Key.Equals(val));
+  similarityScore = similarityScore + (result is null ? 0 : Int64.Parse(val) * result.Count());
+}
+Console.WriteLine(similarityScore);
