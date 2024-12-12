3b22d4bb46632c7c4d7d932234f1aa82463bc525
Tandi Sunarto <tandi.sunarto@vaeit.com>
2024-12-12 03:08:05 -0500

    Finish day 3

---
+++ 2024/day-03/part-1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+using static System.Math;
+
+using StreamReader sr = new StreamReader("data.txt");
+
+var lines = sr.ReadToEnd();
+
+// Console.WriteLine(lines);
+MatchCollection matches = Regex.Matches(lines, @"(mul\((\d{1,3}),(\d{1,3})\))");
+
+Int64 sum = 0;
+foreach(Match match in matches)
+{
+  sum += Int64.Parse(match.Groups[2].Value) * Int64.Parse(match.Groups[3].Value);
+}
+Console.WriteLine("{0}", sum);
---
+++ 2024/day-03/part-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+using StreamReader sr = new StreamReader("data.txt");
+
+string lines = sr.ReadToEnd();
+
+var continueRemove = true;
+int startIndex = 0;
+
+while (continueRemove)
+{
+  int dontIndex = lines.IndexOf("don't()", startIndex);
+  if (dontIndex <= 0) break;
+  int doIndex = lines.IndexOf("do()", dontIndex);
+
+  startIndex = dontIndex;
+  continueRemove = dontIndex > 0 && doIndex > 0 && dontIndex < doIndex;
+   if (continueRemove)
+  {
+    lines = lines.Remove(dontIndex, doIndex - dontIndex + 3);
+  }
+}
+
+MatchCollection matches = Regex.Matches(lines, @"(mul\((\d{1,3}),(\d{1,3})\))");
+
+Int64 sum = 0;
+foreach(Match match in matches)
+{
+  sum += Int64.Parse(match.Groups[2].Value) * Int64.Parse(match.Groups[3].Value);
+}
+Console.WriteLine("{0}", sum);
